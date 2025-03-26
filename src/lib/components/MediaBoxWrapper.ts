e2eb8a0ca7581583e57430aa098d3f7a997d7390
KoloMl <thekolomoets@hotmail.com>
2025-03-26 20:03:04 +0400

    Fixed last media box on the page not being marked as the last in a row

--- src/lib/components/MediaBoxWrapper.ts
+++ src/lib/components/MediaBoxWrapper.ts
@@ -3,6 +3,7 @@ import { getComponent } from "$lib/components/base/component-utils";
 import { buildTagsAndAliasesMap } from "$lib/booru/tag-utils";
 import { on } from "$lib/components/events/comms";
 import { eventTagsUpdated } from "$lib/components/events/maintenance-popup-events";
+import onMessageExternal = chrome.runtime.onMessageExternal;
 
 export class MediaBoxWrapper extends BaseComponent {
   #thumbnailContainer: HTMLElement | null = null;
@@ -89,6 +90,11 @@ export function calculateMediaBoxesPositions(mediaBoxesList: NodeListOf<HTMLElem
 
       lastMediaBox = mediaBoxElement;
       lastMediaBoxPosition = yPosition;
+    }
+
+    // Last-ever media box is checked separately
+    if (lastMediaBox && !lastMediaBox.nextElementSibling) {
+      lastMediaBox.classList.add('media-box--last');
     }
   })
 }
